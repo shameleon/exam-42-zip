a12256475e2a56ac66ae3b4925d57106dec46ccd
jmouaike <112470438+shameleon@users.noreply.github.com>
2023-02-14 22:41:27 +0100

    Update ft_split.c
    make your own ft_split. here is an example of main()

--- ex08/ft_split.c
+++ ex08/ft_split.c
@@ -1,60 +1,15 @@
 /* ************************************************************************** */
 /* split.c
-
-    - 
-    -
-    -
-    -
-    -
 */
 
 #include <stdlib.h>
 #include <stdio.h>
-
-int     is_space(char c)
-{
-    return (c == 32 || (c >= 9 && c <= 13));
-}
-
-char **get_words(warr, s, c)
-{
-    return (warr);
-}
-
-int     count_words(char *s)
-{
-    int     i;
-    int     wc;
-    
-    i = 0;
-    wc = 0;
-    while (s[i] != '\0')
-    {
-        if (i == 0 && !is_space(s[i]))
-            wc++;
-        else if (i > 0 && !is_space(s[i]) && is_space(s[i - 1]))
-            wc++;
-        i++;
-    }
-    return (wc);
-}
-
-char    **ft_split(char *str)
-{
-    char    **warr; 
-    int     wc;
-
-    if (s = NULL)
-        return (NULL);
-    wc = count_words(str);
-    warr = (char *)malloc (sizeof(*warr) * (wc + 1));
-    if (!warr)
-        return (NULL);
-    warr = get_words(warr, s, c);
-    warr[wc] = NULL;
-    return (warr);
-}
-
+/*
+int     is_space(char c);
+char **get_words(warr, s, c);
+int     count_words(char *s);
+char    **ft_split(char *str);
+*/
 int     main(void)
 {
     char    **warr;
@@ -67,7 +22,9 @@ int     main(void)
     while (warr[i])
     {
         printf("%s\n",warr[i]);
+        free (warr[i]);
         i++;
     }
+    free (warr);
     return (0);
 }
